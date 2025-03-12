a73a07e636afca376703212cd205aded8a462fd3
oshanLahiru0307 <klahiruoshan@gmail.com>
2025-03-12 09:29:42 +0530

    login and registering pages are completed

--- frontend/src/Components/Login.jsx
+++ frontend/src/Components/Login.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { Card, message, Button, Checkbox, Form, Input, Typography } from "antd";
+import { Card, message, Button, Checkbox, Form, Input, Typography, Row, Col } from "antd";
 import authController from "../Services/authController";
 
 const { Title, Text } = Typography;
@@ -13,7 +13,6 @@ const Login = () => {
     setLoading(true);
     try {
       const response = await authController.userLogin(values);
-      console.log(response);
       if (response) {
         message.success("Login successful");
         localStorage.setItem("token", response.token);
@@ -36,59 +35,65 @@ const Login = () => {
         justifyContent: "center",
         alignItems: "center",
         height: "100vh",
-        backgroundImage:'url("https://img.freepik.com/free-vector/realistic-abstract-technology-particle-background_23-2148431263.jpg?t=st=1741749623~exp=1741753223~hmac=bbcc38b7a9f7b3917db2d3a2855e231a8269d2eb50d0eb77a10222db37f5e7a0&w=1380")',
+        background: 'url("https://img.freepik.com/free-vector/abstract-smooth-gradient-background-with-blur-effect_1017-51593.jpg?t=st=1741750628~exp=1741754228~hmac=457242daffc9a2f3711276bb2fcf6d6e6b6e72317c11fa5f556e668051e402eb&w=1800")',
         backgroundSize: "cover",
       }}
     >
-      <Card
+      <div
         style={{
-          width: 400,
-          padding: "30px",
-          textAlign: "center",
+          width: '1200px',
+          height: '490px',
+          padding: "0",
           borderRadius: "10px",
+          overflow: "hidden",
+          background: "white",
         }}
       >
-        <Title level={3} style={{ color: "#1890ff" }}>
-          Login Account
-        </Title>
-        <Text type="secondary">
-          Enter your credentials to access your account.
-        </Text>
-        <Form
-          layout="vertical"
-          onFinish={onFinish}
-          style={{ marginTop: 20 }}
-        >
-          <Form.Item
-            label="Email ID"
-            name="email"
-            rules={[{ required: true, message: "Please enter your email!" }]}
-          >
-            <Input />
-          </Form.Item>
+        <Row 
+        style={{}}>
+          {/* Left Side - Image */}
+          <Col span={12} style={{ background: "#1890ff", display: "flex", alignItems: "center", justifyContent: "center" }}>
+            <img
+              src="https://img.freepik.com/free-vector/gradient-minimalist-background_23-2150012324.jpg?t=st=1741750603~exp=1741754203~hmac=e59ec322a8ea4adfa906509c0c764a97edb1e4ece038a4df252533e3acd24e40&w=1380"
+              alt="Login"
+              style={{ width: "100%", height: "100%", objectFit: "cover" }}
+            />
+          </Col>
 
-          <Form.Item
-            label="Password"
-            name="password"
-            rules={[{ required: true, message: "Please enter your password!" }]}
-          >
-            <Input.Password />
-          </Form.Item>
+          {/* Right Side - Form */}
+          <Col span={12} style={{ padding: "40px" }}>
+            <Title level={3} style={{ color: "#1890ff", textAlign: "center" }}>
+              Login Account
+            </Title>
+            <Text type="secondary" style={{ display: "block", textAlign: "center", marginBottom: 20 }}>
+              Enter your credentials to access your account.
+            </Text>
+            <Form layout="vertical" onFinish={onFinish}>
+              <Form.Item label="Email ID" name="email" rules={[{ required: true, message: "Please enter your email!" }]}>
+                <Input />
+              </Form.Item>
 
-          <Form.Item>
-            <Checkbox>Keep me signed in</Checkbox>
-          </Form.Item>
+              <Form.Item label="Password" name="password" rules={[{ required: true, message: "Please enter your password!" }]}>
+                <Input.Password />
+              </Form.Item>
 
-          <Form.Item>
-            <Button type="primary" htmlType="submit" loading={loading} block>
-              Subscribe
-            </Button>
-          </Form.Item>
-          <Text>
-            Already a member? <a href="/register">Sign up</a>
-          </Text>
-        </Form>
-      </Card>
+              <Form.Item>
+                <Checkbox>Keep me signed in</Checkbox>
+              </Form.Item>
+
+              <Form.Item>
+                <Button type="primary" htmlType="submit" loading={loading} block>
+                  Subscribe
+                </Button>
+              </Form.Item>
+
+              <Text type="secondary" style={{ display: "block", textAlign: "center" }}>
+                Not a member? <a href="/register">Sign up</a>
+              </Text>
+            </Form>
+          </Col>
+        </Row>
+      </div>
     </div>
   );
 };
--- frontend/src/Components/Register.jsx
+++ frontend/src/Components/Register.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { Card, message, Button, Form, Input, Typography } from "antd";
+import { Card, message, Button, Form, Input, Typography, Row, Col } from "antd";
 import authController from "../Services/authController";
 
 const { Title, Text } = Typography;
@@ -13,7 +13,6 @@ const Register = () => {
     setLoading(true);
     try {
       const response = await authController.userRegister(values);
-      console.log(response);
       if (response) {
         message.success("Registration successful!");
         navigate("/login");
@@ -34,72 +33,67 @@ const Register = () => {
         justifyContent: "center",
         alignItems: "center",
         height: "100vh",
-        backgroundImage:
-          'url("https://img.freepik.com/free-vector/realistic-abstract-technology-particle-background_23-2148431263.jpg?t=st=1741749623~exp=1741753223~hmac=bbcc38b7a9f7b3917db2d3a2855e231a8269d2eb50d0eb77a10222db37f5e7a0&w=1380")',
+        background: 'url("https://img.freepik.com/free-vector/abstract-smooth-gradient-background-with-blur-effect_1017-51593.jpg?t=st=1741750628~exp=1741754228~hmac=457242daffc9a2f3711276bb2fcf6d6e6b6e72317c11fa5f556e668051e402eb&w=1800")',
         backgroundSize: "cover",
       }}
     >
-      <Card
+      <div
         style={{
-          width: 400,
-          padding: "30px",
-          textAlign: "center",
+          width: '1200px',
+          padding: "0",
           borderRadius: "10px",
+          overflow: "hidden",
+          background: "white",
         }}
       >
-        <Title level={3} style={{ color: "#1890ff" }}>
-          Create an Account
-        </Title>
-        <Text type="secondary">
-          Fill in the details to register your account.
-        </Text>
-        <Form
-          layout="vertical"
-          onFinish={onFinish}
-          style={{ marginTop: 20 }}
-        >
-          <Form.Item
-            label="Full Name"
-            name="name"
-            rules={[{ required: true, message: "Please enter your name!" }]}
-          >
-            <Input />
-          </Form.Item>
+        <Row>
+          {/* Left Side - Image */}
+          <Col span={12} style={{ background: "#1890ff", display: "flex", alignItems: "center", justifyContent: "center" }}>
+            <img
+              src="https://img.freepik.com/free-vector/gradient-minimalist-background_23-2150012324.jpg?t=st=1741750603~exp=1741754203~hmac=e59ec322a8ea4adfa906509c0c764a97edb1e4ece038a4df252533e3acd24e40&w=1380"
+              alt="Register"
+              style={{ width: "100%", height: "100%", objectFit: "cover" }}
+            />
+          </Col>
 
-          <Form.Item
-            label="Email"
-            name="email"
-            rules={[{ required: true, message: "Please enter your email!" }]}
-          >
-            <Input />
-          </Form.Item>
+          {/* Right Side - Form */}
+          <Col span={12} style={{ padding: "40px" }}>
+            <Title level={3} style={{ color: "#1890ff", textAlign: "center" }}>
+              Create an Account
+            </Title>
+            <Text type="secondary" style={{ display: "block", textAlign: "center", marginBottom: 20 }}>
+              Fill in the details to register your account.
+            </Text>
+            <Form layout="vertical" onFinish={onFinish}>
+              <Form.Item label="Full Name" name="name" rules={[{ required: true, message: "Please enter your name!" }]}>
+                <Input />
+              </Form.Item>
 
-          <Form.Item
-            label="Phone Number"
-            name="phone"
-            rules={[{ required: true, message: "Please enter your phone number!" }]}
-          >
-            <Input />
-          </Form.Item>
+              <Form.Item label="Email" name="email" rules={[{ required: true, message: "Please enter your email!" }]}>
+                <Input />
+              </Form.Item>
 
-          <Form.Item
-            label="Password"
-            name="password"
-            rules={[{ required: true, message: "Please enter a password!" }]}
-          >
-            <Input.Password />
-          </Form.Item>
+              <Form.Item label="Phone Number" name="phone" rules={[{ required: true, message: "Please enter your phone number!" }]}>
+                <Input />
+              </Form.Item>
 
-          <Form.Item>
-            <Button type="primary" htmlType="submit" loading={loading} block>
-              Register
-            </Button>
-          </Form.Item>
-          <Text>
-            Already a member? <a href="/login">Sign in</a>
-          </Text>
-        </Form>
-      </Card>
+              <Form.Item label="Password" name="password" rules={[{ required: true, message: "Please enter a password!" }]}>
+                <Input.Password />
+              </Form.Item>
+
+              <Form.Item>
+                <Button type="primary" htmlType="submit" loading={loading} block>
+                  Register
+                </Button>
+              </Form.Item>
+
+              <Text type="secondary" style={{ display: "block", textAlign: "center" }}>
+                Already a member? <a href="/login">Sign in</a>
+              </Text>
+            </Form>
+          </Col>
+        </Row>
+      </div>
     </div>
   );
 };
